ac7c66ffc6b812094c1a5e7a560d54dd91b30b1d
sunsiyan <sun_si_yan@163.com>
2020-06-09 16:16:57 +0800

    lodash

--- src/page/pages/global_plugin/lodash/index.vue
+++ src/page/pages/global_plugin/lodash/index.vue
@@ -1,29 +1,46 @@
 <template>
     <div>
+        <div class="marginBottom20">这是一个lodash页面</div>
+        <el-divider content-position="left">数组方法</el-divider>
         <div class="marginBottom20">A数组：{{JSON.stringify(arrA)}}</div>
         <div class="marginBottom20">B数组：{{JSON.stringify(arrB)}}</div>
-        <el-divider content-position="left">结果</el-divider>
-        <div class="marginBottom20">{{lodash.difference(arrA,arrB)}}</div>
+        <div class="marginBottom20">A数组中与B数组中不同的元素：{{JSON.stringify(arrC)}}</div>
+        <div class="marginBottom20">拆分A数组：{{JSON.stringify(arrD)}}</div>
+        <div class="marginBottom20">过滤假值数据：{{JSON.stringify(arrE)}}</div>
+        <div class="marginBottom20">数组连接：{{JSON.stringify(arrF)}}</div>
     </div>
 </template>
 
 <script>
-    import _ from 'lodash';
+    import lodash from 'lodash';
 
     export default {
         name: "index",
         data() {
             return {
-                arrA: [1, 2, 3],
-                arrB: [2],
-                arrC: []
+                arrA: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, false, null, undefined, NaN, ''],
+                arrB: [2, 7, 10, 13],
+                arrC: [],
+                arrD: [],
+                arrE: [],
+                arrF: [],
+                arrG: [],
+                arrH: [],
             }
         },
         methods: {
             getData() {
                 let _t = this;
-                console.log(_.difference())
-                // _t.arrC = _.difference(arrA, arrB);
+                _t.arrC = lodash.difference(_t.arrA, _t.arrB);
+
+                _t.arrD = lodash.chunk(_t.arrA, 4);
+
+                _t.arrE = lodash.compact(_t.arrA);
+
+
+                _t.arrF = lodash.concat(_t.arrA,_t.arrC);
+
+
             }
         },
         created() {
