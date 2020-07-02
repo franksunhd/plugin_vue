f0a2aca825d41b48475543d4cec6aee482268f67
sunsiyan <sun_si_yan@163.com>
2020-07-02 15:29:14 +0800

    修改首页内容

--- src/page/pages/Home.vue
+++ src/page/pages/Home.vue
@@ -1,18 +1,64 @@
 <template>
     <div class="homeBox">
         <p>本项目主要针对vue常用的一些插件进行展示,并附带了插件对应的API文档.</p>
-        <p>具体包含了markdown在线编辑器, 富文本编辑器, 复制粘贴插件, 生成二维码和条形码的插件</p>
+        <p class="marginBottom20">具体包含了以下插件：</p>
+        <div class="marginBottom20" v-for="(item,index) in menuList" :key="index">
+            <div class="title" v-if="item.name !== '首页'">{{item.name}}</div>
+            <template v-if="item.children">
+                <div @click="clickTo(val)" class="childrenTitle" v-for="(val,i) in item.children" :key="i">
+                    {{val.name}}
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
 <script>
+    import menu from "../../assets/js/menu";
+
     export default {
-        name: 'Home'
+        name: 'Home',
+        data() {
+            return {
+                menuList: []
+            }
+        },
+        methods: {
+            // 点击跳转页面
+            clickTo(val) {
+                this.$router.push({path: val.path});
+            }
+        },
+        created() {
+            this.menuList = menu.menuList;
+        }
     };
 </script>
 
 <style scoped>
     .homeBox {
         line-height: 30px;
+    }
+
+    .homeBox p {
+        font-size: 20px;
+    }
+
+    .title,
+    .childrenTitle {
+        line-height: 40px;
+        height: 40px;
+        font-size: 24px;
+    }
+
+    .title {
+        color: #2D64B3;
+    }
+
+    .childrenTitle {
+        display: inline-block;
+        margin-left: 30px;
+        color: #9cb945;
+        cursor: pointer;
     }
 </style>
